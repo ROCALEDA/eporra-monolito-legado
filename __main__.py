ab5dbff3d91e6b73b65628ff9ab8e21c0d924559
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-06-17 00:49:33 -0500

    Adjusting missing unused package and gitignore

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from src.logica.eporra import EPorra
 from src.vista.InterfazEPorra import App_EPorra
-from src.logica.Logica_mock import Logica_mock
+#from src.logica.Logica_mock import Logica_mock
 if __name__ == '__main__':
     # Punto inicial de la aplicación
 
