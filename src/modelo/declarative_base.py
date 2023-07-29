fa43bcb88c02f11b49bc162ed84198f093bfcf57
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-07-28 23:20:27 -0400

    Se agrega conexion a bd postgres

--- src/modelo/declarative_base.py
+++ src/modelo/declarative_base.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-engine = create_engine('sqlite:///aplicacion.sqlite')
+# Cargar las variables de entorno del archivo .env
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
